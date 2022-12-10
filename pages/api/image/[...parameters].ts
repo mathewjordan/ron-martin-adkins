fb11bea9d8a591545da9530a2b8a7dece0bb989b
Mat Jordan <mat@northwestern.edu>
2022-12-10 08:11:48 -0500

    Troubleshoot asset environment pathing.

--- pages/api/image/[...parameters].ts
+++ pages/api/image/[...parameters].ts
@@ -2,15 +2,16 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import IIIF from "iiif-processor";
 import absoluteUrl from "next-absolute-url";
 import { createReadStream } from "fs";
+import { resolve } from "path";
 
-const streamResolver = ({ id }) => {
-  let imagePath = `public/images/${id}.jpg`;
+const streamResolver = ({ id }: { id: string; baseUrl?: string }) => {
+  const imagePath = resolve(process.cwd(), `assets/images/${id}.jpg`);
   return createReadStream(imagePath);
 };
 
 const handler = async (request: NextApiRequest, response: NextApiResponse) => {
   const { origin } = absoluteUrl(request);
-  const url = origin.concat(request.url);
+  const url = origin.concat(request.url as string);
 
   const processor = new IIIF.Processor(url, streamResolver, {
     pathPrefix: "/api/image/",
